1dadb9e7100d6fb39ff68be40dc8eadd5e3b7155
Arthy22 <130374738+Arthy22@users.noreply.github.com>
2024-07-29 22:35:27 +0530

    day 23

---
+++ dsa_q19.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid, int i, int j){
+        if(i<0 || i>=grid.size() || j<0 ||j>=grid[0].size() || grid[i][j]=='0'){
+            return;
+        }
+        grid[i][j]='0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        int count=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1'){
+                    dfs(grid,i,j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
